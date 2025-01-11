37b8fdd4ae195421806b54299acae4ae8007ea53
Farris Matar <farrismatar@gmail.com>
2025-01-11 13:58:48 -0500

    finished code for UART buffer I2C driver
    - Implements an interrupt to determine when to read buffer data
    - Buffer data is read until buffer no longer contains any more barcodes
    - Barcodes are printed to console, indicating which scanner they're from

--- STM32 Nucleo/Barcode Scanner/Core/Src/main.c
+++ STM32 Nucleo/Barcode Scanner/Core/Src/main.c
@@ -34,12 +34,26 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+// XR20M slave address & internal register addresses
 #define XR20M_ADDR 0x60 << 1
+#define BUF_RHR_THR_DLL 		0x00
+#define BUF_DLM_IER 			0x01
+#define BUF_DLD_ISR_FCR_EFR 	0x02
+#define BUF_LCR 				0x03
+#define BUF_MCR_XON1 			0x04
+#define BUF_LSR_XON2			0x05
+#define BUF_MSR_TCR_XOFF1		0x06
+#define BUF_SPR_TLR_XOFF2		0x07
+#define BUF_TXLVL				0x08
+#define BUF_RXLVL				0x09
+#define BUF_IODIR				0x0A
+#define BUF_IOSTATE				0x0B
+#define BUF_IOINTEN				0x0C
+#define BUF_IOCTRL				0x0E
+#define BUF_EFCR				0x0F
 
-#define BUF_RHR_THR_DLL 0x00
-#define BUF_DLM_IER 0x01
-#define BUF_DLD_ISR_FCR_EFR 0x02
-#define BUF_LCR 0x03
+// Barcode length constant (standard for grocery stores is 12 numbers + end-of-line character)
+#define BARCODE_LEN 			13
 
 /* USER CODE END PD */
 
@@ -55,6 +69,8 @@ UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+// Global variables - used for ISRs to raise flags
+bool bufferDataFlag = 0;
 
 /* USER CODE END PV */
 
@@ -88,42 +104,48 @@ int _write(int file, char *data, int len) {
 int main(void)
 {
 
-	/* USER CODE BEGIN 1 */
-	uint8_t rxData[100] = {0};
+  /* USER CODE BEGIN 1 */
+	char rxData[BARCODE_LEN + 1] = {0};
 	uint8_t rxI2C[64] = {0};
 	uint8_t txI2C[64] = {0};
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_USART2_UART_Init();
-	MX_USART1_UART_Init();
-	MX_I2C1_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
 	memset(rxData, 0 ,sizeof(rxData));
 
 	// Configuring the XR20M1172 buffer on startup
-	// Starting with line control register (LCR), also gives access to divisor registers to be configured
+	// Starting with line control register (LCR) to get access to enhanced features for config
+	BufferRegisterWrite(txI2C, BUF_LCR, 0xBF, 0);
+	BufferRegisterWrite(txI2C, BUF_LCR, 0xBF, 1);
+	// Enabling enhanced features for both channels
+	BufferRegisterWrite(txI2C, BUF_DLD_ISR_FCR_EFR, 0x10, 0);
+	BufferRegisterWrite(txI2C, BUF_DLD_ISR_FCR_EFR, 0x10, 1);
+
+	// Configure UART with LCR, also gives access to divisor registers to be configured
 	BufferRegisterWrite(txI2C, BUF_LCR, 0x83, 0); // Channel A configured to 8-bit word length, no parity, 1 stop bit, divisor registers enabled
 	BufferRegisterWrite(txI2C, BUF_LCR, 0x83, 1); // Channel B configured to 8-bit word length, no parity, 1 stop bit, divisor registers enabled
-
 	// Configure clock divisors - set baud rate for channel A and B to 9600 (based on 24MHz crystal)
 	BufferRegisterWrite(txI2C, BUF_RHR_THR_DLL, 0x9C, 0);
 	BufferRegisterWrite(txI2C, BUF_DLD_ISR_FCR_EFR, 0x04, 0);
@@ -131,13 +153,28 @@ int main(void)
 	BufferRegisterWrite(txI2C, BUF_RHR_THR_DLL, 0x9C, 1);
 	BufferRegisterWrite(txI2C, BUF_DLD_ISR_FCR_EFR, 0x04, 1);
 	BufferRegisterWrite(txI2C, BUF_DLM_IER, 0x00, 1);
+	// Re-configure LCR to change access from divisor registers to other config registers
+	BufferRegisterWrite(txI2C, BUF_LCR, 0x03, 0);
+	BufferRegisterWrite(txI2C, BUF_LCR, 0x03, 1);
 
-	/* USER CODE END 2 */
+	// Enable FIFO mode for both channels
+	BufferRegisterWrite(txI2C, BUF_DLD_ISR_FCR_EFR, 0x01, 0);
+	BufferRegisterWrite(txI2C, BUF_DLD_ISR_FCR_EFR, 0x01, 1);
+	// Enable the trigger level registers
+	BufferRegisterWrite(txI2C, BUF_MCR_XON1, 0x04, 0);
+	BufferRegisterWrite(txI2C, BUF_MCR_XON1, 0x04, 1);
+	// Set the trigger level to 12 characters (used for interrupt generation)
+	BufferRegisterWrite(txI2C, BUF_SPR_TLR_XOFF2, 0x03, 0);
+	BufferRegisterWrite(txI2C, BUF_SPR_TLR_XOFF2, 0x03, 1);
+	// Enable the interrupt for the receive buffer trigger level
+	BufferRegisterWrite(txI2C, BUF_DLM_IER, 0x01, 0);
+	BufferRegisterWrite(txI2C, BUF_DLM_IER, 0x01, 1);
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
-	while (1)
-	{
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1) {
 	  // Wait for message over UART (blocking mode)
 //	  if (HAL_UART_Receive(&huart1, rxData, sizeof(rxData), HAL_MAX_DELAY) == HAL_OK) {
 //		  // Print received message
@@ -146,12 +183,45 @@ int main(void)
 //		  // Clear memory buffer for next message
 //		  memset(rxData, 0 ,sizeof(rxData));
 //	  }
-		HAL_Delay(1000);
-		printf("Hello world!\r\n");
+//		HAL_Delay(1000);
+//		printf("Hello world!\r\n");
 
-		/* USER CODE END WHILE */
+		// Check if an interrupt was received indicating data is available from the buffer
+		if (bufferDataFlag) {
+			bufferDataFlag = 0; // Reset the flag
 
-		/* USER CODE BEGIN 3 */
+			// Check the amount stored in each buffer - if a full barcode is stored (12 characters + end-of-line character), read it from the FIFO buffer
+			BufferRegisterRead(rxI2C, BUF_RXLVL, 1, 0);
+			// Keep reading barcodes as long as the buffer contains enough characters
+			while (rxI2C[0] >= BARCODE_LEN) {
+				BufferRegisterRead(rxI2C, BUF_RHR_THR_DLL, BARCODE_LEN, 0);
+				// Format the recevied data into a character array
+				for (int i = 0; i < BARCODE_LEN; i++) {
+					rxData[i] = (char)rxI2C[i];
+				}
+				rxData[BARCODE_LEN] = '\0'; // Add null-terminator to end the string
+
+				printf("Received barcode from scanner A: %s\n", rxData);
+				// Re-read the buffer level to check if any barcodes remaining
+				BufferRegisterRead(rxI2C, BUF_RXLVL, 1, 0);
+			}
+			BufferRegisterRead(rxI2C, BUF_RXLVL, 1, 1);
+			while (rxI2C[0] >= BARCODE_LEN) {
+				BufferRegisterRead(rxI2C, BUF_RHR_THR_DLL, BARCODE_LEN, 1);
+				for (int i = 0; i < BARCODE_LEN; i++) {
+					rxData[i] = (char)rxI2C[i];
+				}
+				rxData[BARCODE_LEN] = '\0';
+
+				printf("Received barcode from scanner B: %s\n", rxData);
+				// Re-read the buffer level to check if any barcodes remaining
+				BufferRegisterRead(rxI2C, BUF_RXLVL, 1, 1);
+			}
+		}
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
 	}
   /* USER CODE END 3 */
 }
@@ -328,12 +398,22 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
@@ -342,6 +422,10 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 /**
  * Helper function to handle writing to one of the buffer's internal UART registers
+ * @param txBytes Pointer to 8-bit integer array to transmit over I2C
+ * @param regAddr Internal register address to write to
+ * @param regData Data to write to the internal register
+ * @param selChB Boolean for selecting channel A or B to write to
  */
 void BufferRegisterWrite(uint8_t *txBytes, uint8_t regAddr, uint8_t regData, bool selChB) {
 	// Construct address by shifting the register address and channel select appropriately
@@ -355,6 +439,10 @@ void BufferRegisterWrite(uint8_t *txBytes, uint8_t regAddr, uint8_t regData, boo
 
 /**
  * Helper function to handle reading from one of the buffer's internal UART registers
+ * @param rxBytes Pointer to 8-bit integer array to receive I2C data
+ * @param regAddr Internal register address to read from
+ * @param numBytes Number of bytes to read from the register
+ * @param selChB Boolean for selecting channel A or B to write to
  */
 void BufferRegisterRead(uint8_t *rxBytes, uint8_t regAddr, uint8_t numBytes, bool selChB) {
 	// Construct address by shifting the register address and channel select appropriately
@@ -367,6 +455,19 @@ void BufferRegisterRead(uint8_t *rxBytes, uint8_t regAddr, uint8_t numBytes, boo
 	HAL_I2C_Master_Receive(&hi2c1, XR20M_ADDR, rxBytes, numBytes, HAL_MAX_DELAY);
 }
 
+//void EXTI4_IRQHandler(void) {
+//    // Clear the EXTI interrupt flag
+//    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
+//    // Raise a flag to indicate the buffer has data to be received
+//    bufferDataFlag = 1;
+//}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    if (GPIO_Pin == GPIO_PIN_4) {
+    	// Raise a flag to indicate the buffer has data to be received
+        bufferDataFlag = 1;
+    }
+}
 
 /* USER CODE END 4 */
 
